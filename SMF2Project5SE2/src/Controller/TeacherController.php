4dced350a2838ac4ec8cc7390eea2002aadb70e6
amenir88 <ameni.rommene@esprit.tn>
2021-09-30 00:27:56 +0100

    first commit

---
+++ SMF2Project5SE2/src/Controller/TeacherController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    /**
+     * @Route("/teacher", name="teacher")
+     */
+    public function index(): Response
+    {
+        return $this->render('teacher/index.html.twig', [
+            'controller_n' => 'Test', 'cn'=>'testcn'
+        ]);
+    }
+
+
+}
