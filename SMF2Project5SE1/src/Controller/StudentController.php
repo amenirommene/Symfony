125da8516ca3597b54b39c646936a4075e2bae5e
amenir88 <ameni.rommene@esprit.tn>
2021-10-01 16:01:00 +0100

    second commit : twig

--- SMF2Project5SE1/src/Controller/StudentController.php
+++ SMF2Project5SE1/src/Controller/StudentController.php
@@ -15,8 +15,19 @@ class StudentController extends AbstractController
     public function index(): Response
     {
         //return  new Response("Bonjour mes étudiants");
-        return $this->render('student/index.html.twig',
-            ['controller_name' => 'StudentController', "classe"=>"5SE1"]);
+       // return $this->render('student/index.html.twig',['param1'=>'5SE1', 'param2'=>'Symfony']);
+        $formations = array(
+            array('ref' => 'form147', 'Titre' => 'Formation Symfony 4','Description'=>'formation pratique',
+                'date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020', 'nb_participants'=>19) ,
+            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+                'Description'=>'formation theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+                'nb_participants'=>0),
+            array('ref'=>'form178','Titre'=>'Formation Angular' ,
+                'Description'=>'formation theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020',
+                'nb_participants'=>20));
+
+        return $this->render('club/list.html.twig', ["mesFormations"=>$formations]);
+
     }
     /**
      * @Route("/show/{name}", name="showstudent")
@@ -26,5 +37,22 @@ class StudentController extends AbstractController
         //return  new Response("Bonjour mes étudiants");
         return $this->render('student/student.html.twig',
             ["pathparam"=> $name]);
+    }
+    /**
+     * @Route("/list", name="list")
+     */
+    public function getFormations(){
+        $formations = array(
+            array('ref' => 'form147', 'Titre' => 'Formation Symfony 4','Description'=>'formation pratique',
+                'date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020', 'nb_participants'=>19) ,
+            array('ref'=>'form177','Titre'=>'Formation SOA' ,
+                'Description'=>'formation theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+                'nb_participants'=>0),
+            array('ref'=>'form178','Titre'=>'Formation Angular' ,
+                'Description'=>'formation theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020',
+                'nb_participants'=>20));
+
+        return $this->render('club/list.html.twig', ["mesFormations"=>$formations]);
+
     }
 }
