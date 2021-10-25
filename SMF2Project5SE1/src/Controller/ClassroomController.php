918876a0b096e10bdb918beff7761cc042d4a063
amenir88 <ameni.rommene@esprit.tn>
2021-10-25 08:46:00 +0100

    S6

--- SMF2Project5SE1/src/Controller/ClassroomController.php
+++ SMF2Project5SE1/src/Controller/ClassroomController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Classroom;
+use App\Form\ClassroomType;
+use App\Repository\ClassroomRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,5 +24,74 @@ class ClassroomController extends AbstractController
         return $this->render('classroom/index.html.twig', [
             'list' => $listc
         ]);
+    }
+    /**
+     * @Route("/listclassroom2", name="listclassroom2")
+     */
+    public function index2(ClassroomRepository $repo): Response
+    {
+        $listc=$repo->findAll();
+        return $this->render('classroom/index.html.twig', [
+            'list' => $listc
+        ]);
+    }
+    /**
+     * @Route("/addC", name="addC")
+     */
+    public function addC(): Response
+    {
+
+        $cl=new Classroom();
+        $cl->setName('4SE1');
+        $cl1=new Classroom();
+        $cl1->setName('4SE2');
+        $manager=$this->getDoctrine()->getManager();
+        $manager->persist($cl);
+        $manager->persist($cl1);
+        $manager->flush();
+        return $this->redirectToRoute("listclassroom2");
+        /* return $this->render('classroom/index.html.twig', [
+            'list' => $listc
+        ]);*/
+
+    }
+
+    /**
+     * @Route("/addClassroom", name="addClassroom")
+     */
+    public function addClassroom(Request $request): Response
+    {
+
+        $cl=new Classroom();
+        $myForm=$this->createForm(ClassroomType::class,$cl);
+        $myForm=$myForm->handleRequest($request);
+        $myForm->add('Ajouter', SubmitType::class);
+        if ($myForm->isSubmitted()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($cl);
+            $manager->flush();
+            return $this->redirectToRoute("listclassroom2");
+        }
+        return $this->render('classroom/add.html.twig',['monF'=>$myForm->createView()]);
+
+    }
+
+    /**
+     * @Route("/updateC/{id}", name="updateClassroom")
+     */
+    public function updateClassroom(ClassroomRepository $repo,$id, Request $request): Response
+    {
+        $cl=$repo->find($id);
+        $myForm=$this->createForm(ClassroomType::class,$cl);
+        $myForm->handleRequest($request);
+        $myForm->add('Modifier', SubmitType::class);
+        if ($myForm->isSubmitted()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($cl);
+            $manager->flush();
+            return $this->redirectToRoute("listclassroom2");
+        }
+        return $this->render('classroom/add.html.twig',['monF'=>$myForm->createView()]);
+
     }
 }
--- SMF2Project5SE1/src/Controller/StudentController.php
+++ SMF2Project5SE1/src/Controller/StudentController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Classroom;
+use App\Entity\Student;
+use App\Form\StudentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,6 +31,39 @@ class StudentController extends AbstractController
         return $this->render('student/student.html.twig',
             ["pathparam"=> $name]);
     }
+    /**
+     * @Route("/student/get", name="getStudents")
+     */
+    public function getStudents(Request $request): Response
+    {
+        $repo=$this->getDoctrine()->getRepository(Student::class);
+        $students=$repo->findAll();
+        return $this->render("student/list.html.twig", ["students"=>$students]);
+    }
 
+    /**
+     * @Route("/student/add", name="addStudent")
+     */
+    public function addStudent(Request $request): Response
+    {
 
+        //instance de student dans lequel je vais stocker les données de mon formulaire
+        $st=new Student();
+        /*$email=$request->get('email');
+         * $nsc=$request->get('nsc');
+         * $classe=$request->get('classe');
+         * $st->setEmail($email);
+         */
+        //créer le formulaire
+        $form=$this->createForm(StudentType::class,$st);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+             $manager=$this->getDoctrine()->getManager();
+             $manager->persist($st);
+             $manager->flush();
+            return $this->redirectToRoute("getStudents");
+        }
+        return $this->render('student/add.html.twig',
+            ["myForm"=> $form->createView()]);
+    }
 }
