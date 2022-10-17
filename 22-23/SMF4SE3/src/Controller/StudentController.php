b66d365a9b6c47ebcb355133f847b5b398561c1c
amenir88 <ameni.rommene@esprit.tn>
2022-10-17 16:49:15 +0100

    formulaire

--- 22-23/SMF4SE3/src/Controller/StudentController.php
+++ 22-23/SMF4SE3/src/Controller/StudentController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 
 use App\Entity\Student;
 use App\Form\StudentType;
+use App\Repository\StudentRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,11 +23,24 @@ class StudentController extends AbstractController
         ]);
     }
 
+    #[Route('/getliststudent', name: 'app_list_student')]
+    public function list(StudentRepository $repo): Response
+    {
+        $students=$repo->findAll();
+        return $this->render("student/list.html.twig", ['students'=>$students]);
+    }
+
     #[Route('/addstudent', name: 'app_add_student')]
-    public function add(ManagerRegistry $doctrine): Response
+    public function add(ManagerRegistry $doctrine, Request $request): Response
     {
         $student=new Student();
         $form=$this->createForm(StudentType::class,$student );
+        $form->handleRequest($request);
+        if ($form->isSubmitted()){
+            $em=$doctrine->getManager();
+            $em->persist($student);
+            $em->flush();
+        }
         return $this->renderForm("student/add.html.twig", ['f'=>$form]);
 
     }
